99ab55a1135caab2242018801e41ff577b1f3209
Pedro Gameiro <pedro.a.gameiro@tecnico.ulisboa.pt>
2016-11-02 14:35:41 +0000

    Drivers for the old mote version. Remove debug led blinking.

--- hardware/msp430/variants/moteist_v0413/Board.mk
+++ hardware/msp430/variants/moteist_v0413/Board.mk
@@ -3,7 +3,8 @@
 ARCH = msp430
 MCU = msp430f5438a
 MCU_FLAG = -mmcu=$(MCU)
-F_CPU = 25000000L
+#F_CPU = 25000000L
+F_CPU = 8000000L
 FLASH_SIZE = 130048
 UPLOAD_COMMAND = $(MSPDEBUG) $(VERBOSE_UPLOAD) tilib --force-reset "prog build/$(SKETCH_NAME).bin"
 ######################################
